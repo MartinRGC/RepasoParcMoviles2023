90a5f9f0c6c8ce8159a25fce7bd924610d878e37
Martin Garcia <martingarcia08.00@gmail.com>
2023-10-11 19:25:47 -0500

    Initial commit

---
+++ app/src/main/java/com/repasoparcmoviles2023/LoginActivity.kt
@@ -0,0 +1,43 @@
+package com.repasoparcmoviles2023
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var edtemail : EditText
+    lateinit var edtpassword : EditText
+    lateinit var btnIngresar : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        edtemail = findViewById(R.id.edtemail)
+        edtpassword = findViewById(R.id.edtpassword)
+        btnIngresar = findViewById(R.id.btnIngresar)
+
+        btnIngresar.setOnClickListener {
+
+
+            var log = Intent(this, LoginActivity::class.java)
+            val email = edtemail.text.toString()
+            val password = edtpassword.text.toString()
+            val intent = Intent(this, MainActivity::class.java)
+            fun validate(email: String,password: String) {
+
+                if (email == "admin@smartapp.com" && password == "admin123"){
+
+                    startActivity(intent)
+
+            }else{
+                    startActivity(log)
+
+               }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/repasoparcmoviles2023/ui/fragments/GeneracionesFragment.kt
@@ -0,0 +1,22 @@
+package com.repasoparcmoviles2023.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.repasoparcmoviles2023.R
+
+
+class GeneracionesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_generaciones, container, false)
+    }
+
+
+}
